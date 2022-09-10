64d00b2705a29b52e2303d26b0691692533dbc41
eplq <91148468+eplq@users.noreply.github.com>
2022-09-10 19:20:13 +0200

    CodeWars decode_the_morse_code_part_1 Python

---
+++ CodeWars/decode_the_morse_code_part_1.py
@@ -0,0 +1,120 @@
+"""
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+In addition to letters, digits and some punctuation, there are some special service codes, the most notorious of those is the international distress signal SOS (that was first issued by Titanic), that is coded as ···−−−···. These special codes are treated as single special characters, and usually are transmitted as separate words.
+
+Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+
+For example:
+
+decode_morse('.... . -.--   .--- ..- -.. .')
+#should return "HEY JUDE"
+
+NOTE: For coding purposes you have to use ASCII characters . and -, not Unicode characters.
+
+The Morse code table is preloaded for you as a dictionary, feel free to use it:
+
+    Coffeescript/C++/Go/JavaScript/Julia/PHP/Python/Ruby/TypeScript: MORSE_CODE['.--']
+    C#: MorseCode.Get(".--") (returns string)
+    F#: MorseCode.get ".--" (returns string)
+    Elixir: @morse_codes variable (from use MorseCode.Constants). Ignore the unused variable warning for morse_codes because it's no longer used and kept only for old solutions.
+    Elm: MorseCodes.get : Dict String String
+    Haskell: morseCodes ! ".--" (Codes are in a Map String String)
+    Java: MorseCode.get(".--")
+    Kotlin: MorseCode[".--"] ?: "" or MorseCode.getOrDefault(".--", "")
+    Racket: morse-code (a hash table)
+    Rust: MORSE_CODE
+    Scala: morseCodes(".--")
+    Swift: MorseCode[".--"] ?? "" or MorseCode[".--", default: ""]
+
+    C: provides parallel arrays, i.e. morse[2] == "-.-" for ascii[2] == "C"
+
+    NASM: a table of pointers to the morsecodes, and a corresponding list of ascii symbols
+
+All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions. In C#, tests will fail if the solution code throws an exception, please keep that in mind. This is mostly because otherwise the engine would simply ignore the tests, resulting in a "valid" solution.
+
+Good luck!
+
+After you complete this kata, you may try yourself at Decode the Morse code, advanced.
+"""
+
+MORSE_CODE: dict
+def decode_morse(morse_code):
+    # remove spaces and split words    
+    words = morse_code \
+        .strip() \
+        .split("   ")
+
+    translated_words = [] # empty array to store translated words
+    for word in words:
+        # separate chars and translate them
+        characters = [MORSE_CODE[morse_char] for morse_char in word.split(" ")]
+        # join translated characters and append them to translated_words
+        translated_words.append("".join(characters))
+
+    # join them into a string
+    return " ".join(translated_words)
+
+
+"""
+The provided dictionary
+MORSE_CODE = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    ".-.-.-": ".",
+    "--..--": ",",
+    "..--..": "?",
+    ".----.": "'",
+    "-.-.--": "!",
+    "-..-.": "/",
+    "-.--.": "(",
+    "-.--.-": ")",
+    ".-...": "&",
+    "---...": ":",
+    "-.-.-.": ";",
+    "-...-": "=",
+    ".-.-.": "+",
+    "-....-": "-",
+    "..--.-": "_",
+    ".-..-.": '"',
+    "...-..-": "$",
+    ".--.-.": "@",
+    "...---...": "SOS",
+}
+"""
